e01575bd57fdfb9b5115d7afa765b6825840626a
mpatel3 <14mcei14@nirmauni.ac.in>
2021-01-16 15:53:35 +0530

    changed_method_to_https

--- redux-city-temprature/src/actions/index.js
+++ redux-city-temprature/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'b4dd75144ff6e5a35c3b69d197ec277a';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const fetchWeather = (city) => {
     const url = `${ROOT_URL}&q=${city},ind`; // state code can be changed.
